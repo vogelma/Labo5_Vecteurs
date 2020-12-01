bf380b93e2435c2d1046f504e0fcc1bad5f2f0d6
Maelle <maelle.vogel@heig-vd.ch>
2020-12-01 09:14:19 +0100

    operator

--- library.cpp
+++ library.cpp
@@ -1,4 +1,47 @@
-//
-// Created by Maëlle on 01.12.2020.
-//
+/*-----------------------------------------------------------------------------------
+Nom du fichier : main.cpp
+Auteur(s)      : Maëlle Vogel et Tobie Praz
+Date creation  : 01.12.2020
+Description    : <à compléter>
+Remarque(s)    : <à compléter>
+Compilateur    : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------*/
+#include "library.h"
 
+#include <iostream>
+#include <string>
+#include <limits>
+#include <ctime>
+#include <cctype>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+//-------------------------------------------------------------------
+//    VECTOR 1D or 2D
+//-------------------------------------------------------------------
+ostream& operator << (ostream& os, const VecteurVector& v) {
+    cout << "(";
+    for (size_t i=0; i < v.size(); ++i) {
+        if (i)
+            os << ", ";
+        os << v[i];
+    }
+    cout << ")";
+    return os;
+}
+
+//-------------------------------------------------------------------
+ostream& operator << (ostream& os, const MatriceVector& m) {
+    cout << "[";
+    for (size_t i=0; i < m.size(); ++i) {
+        if (i)
+            os << ", ";
+        os << m[i];
+    }
+    cout << "]";
+    return os;
+}
--- library.h
+++ library.h
@@ -5,4 +5,16 @@
 #ifndef LABO5_VECTEURS_LIBRARY_H
 #define LABO5_VECTEURS_LIBRARY_H
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+using VecteurVector = vector<int>;
+using MatriceVector = vector<VecteurVector>;
+
+ostream& operator << (ostream& os, const VecteurVector& v);
+ostream& operator << (ostream& os, const MatriceVector& m);
+
+
 #endif //LABO5_VECTEURS_LIBRARY_H
